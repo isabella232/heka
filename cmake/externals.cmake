32653c09650e94bb265f1242298a54fce9cab350
Yash <yash@lyft.com>
2017-11-23 17:48:26 -0800

    local fork of lua sandbox

--- cmake/externals.cmake
+++ cmake/externals.cmake
@@ -18,8 +18,8 @@ if(INCLUDE_SANDBOX)
     set(SANDBOX_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=${PROJECT_PATH} -DLUA_JIT=off --no-warn-unused-cli)
     externalproject_add(
         ${SANDBOX_PACKAGE}
-        GIT_REPOSITORY https://github.com/mozilla-services/lua_sandbox.git
-        GIT_TAG v1.2.7
+        GIT_REPOSITORY https://github.com/lyft/lua_sandbox.git
+        GIT_TAG 92b4725d18c0e76e004eebf4772bf7abedededb5
         CMAKE_ARGS ${SANDBOX_ARGS}
         INSTALL_DIR ${PROJECT_PATH}
     )
